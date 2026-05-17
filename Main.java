b14dc84181add74c4c21521f1653d66376be7e70
nadhifiatalitha <nadhifiatalitha@gmail.com>
2026-05-17 15:32:17 +0700

    Bug Hunting & Simulasi Error

--- Main.java
+++ Main.java
@@ -1,28 +1,30 @@
 interface Exportable {
     void exportData();
 }
-class Produk implements Exportable{
+
+class Produk implements Exportable {
     int IdProduk;
     String NamaProduk;
     String Deskripsi;
     double Harga;
     int JumlahStok;
 
-    public Produk (int IdProduk, String NamaProduk, String Deskripsi, double Harga, int JumlahStok ){
-    this.IdProduk = IdProduk;
-    this.NamaProduk = NamaProduk;
-    this.Deskripsi = Deskripsi;
-    this.Harga = Harga;
-    this.JumlahStok = JumlahStok;
+    public Produk(int IdProduk, String NamaProduk, String Deskripsi, double Harga, int JumlahStok) {
+        this.IdProduk = IdProduk;
+        this.NamaProduk = NamaProduk;
+        this.Deskripsi = Deskripsi;
+        this.Harga = Harga;
+        this.JumlahStok = JumlahStok;
     }
 
-    public void tampilkanProduk(){
+    public void tampilkanProduk() {
         System.out.println("== Detail Produk==");
         System.out.println("Nama:  " + this.NamaProduk);
         System.out.println("Deskripsi:  " + this.Deskripsi);
         System.out.println("Harga:  " + this.Harga);
         System.out.println("Stok:  " + this.JumlahStok);
     }
+
     @Override
     public void exportData() {
         System.out.println("Exporting produk: " + NamaProduk + " - " + Harga);
@@ -33,8 +35,8 @@ class Kategori extends Produk {
     private String namaKategori;
     private String kodeKategori;
 
-    public Kategori(int IdProduk, String NamaProduk, String Deskripsi, double Harga, int JumlahStok, 
-                    String namaKategori, String kodeKategori) {
+    public Kategori(int IdProduk, String NamaProduk, String Deskripsi, double Harga, int JumlahStok,
+            String namaKategori, String kodeKategori) {
         super(IdProduk, NamaProduk, Deskripsi, Harga, JumlahStok);
         this.namaKategori = namaKategori;
         this.kodeKategori = kodeKategori;
@@ -60,17 +62,18 @@ class Rekomendasi extends Produk {
     private String SkorKepercayaan;
     private String Sumber;
 
-    public Rekomendasi(int IdProduk, String NamaProduk, String Deskripsi, double Harga, int JumlahStok, 
-                       int IdRekomendasi, String NamaRekomendasi, int IdPengguna, String SkorKepercayaan, String Sumber){
+    public Rekomendasi(int IdProduk, String NamaProduk, String Deskripsi, double Harga, int JumlahStok,
+            int IdRekomendasi, String NamaRekomendasi, int IdPengguna, String SkorKepercayaan, String Sumber) {
 
-        super(IdProduk, NamaProduk, Deskripsi, Harga, JumlahStok);                
+        super(IdProduk, NamaProduk, Deskripsi, Harga, JumlahStok);
         this.IdRekomendasi = IdRekomendasi;
         this.NamaRekomendasi = NamaRekomendasi;
         this.IdPengguna = IdPengguna;
         this.SkorKepercayaan = SkorKepercayaan;
         this.Sumber = Sumber;
     }
-    public void tampilkanRekomendasi(){
+
+    public void tampilkanRekomendasi() {
         super.tampilkanProduk();
         System.out.println("IdRekomendasi:  " + this.IdRekomendasi);
         System.out.println("Nama Rekomendasi:  " + this.NamaRekomendasi);
@@ -78,31 +81,40 @@ class Rekomendasi extends Produk {
         System.out.println("Skor Kepercayaan:  " + this.SkorKepercayaan);
         System.out.println("Sumber:  " + this.Sumber);
     }
+
     @Override
     public void exportData() {
         System.out.println("Exporting rekomendasi: " + NamaRekomendasi + " - " + SkorKepercayaan);
     }
 }
+
 class InfoPrinter {
     public void printInfo(Exportable item) {
         item.exportData();
     }
 }
+
 public class Main {
     public static void main(String[] args) {
-       
-        Kategori kat1 = new Kategori(2, "Cangkul Baja", "Alat gali tanah", 
-                                     75000, 50, "Alat Pertanian", "ALT-01");
-        
+
+        Kategori kat1 = new Kategori(2, "Cangkul Baja", "Alat gali tanah",
+                75000, 50, "Alat Pertanian", "ALT-01");
+
         kat1.tampilkanKategori();
 
-        Rekomendasi rek1 = new Rekomendasi(1, "Pupuk NPK", "Untuk Tanaman", 
-                                           15000, 100, 1, "Rekomendasi Subur", 10, "80%", "AI");
+        Rekomendasi rek1 = new Rekomendasi(1, "Pupuk NPK", "Untuk Tanaman",
+                15000, 100, 1, "Rekomendasi Subur", 10, "80%", "AI");
         rek1.tampilkanRekomendasi();
 
-        Exportable[] items = {kat1, rek1};
+        Exportable[] items = { kat1, rek1 };
         for (Exportable item : items) {
             item.exportData();
         }
+        Kategori katNull = new Kategori(3, null, null, 0, 0, null, null);
+        System.out.println("Panjang nama: " + katNull.NamaProduk.length());
+        String inputHarga = "dua ratus ribu";
+        double hargaParsed = Double.parseDouble(inputHarga);
+        Produk p = new Produk(4, "Cangkul Mini", "Alat kecil", hargaParsed, 5);
+        p.tampilkanProduk();
     }
 }
